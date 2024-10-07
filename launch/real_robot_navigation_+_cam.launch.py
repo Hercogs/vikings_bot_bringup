c8d460c6362d33b021fabdf0f243ad723d76ad02
Kriss213 <kriss.vasermanslv@gmail.com>
2024-10-07 11:21:57 +0300

    changed display manager node package name

--- launch/real_robot_navigation_+_cam.launch.py
+++ launch/real_robot_navigation_+_cam.launch.py
@@ -274,7 +274,7 @@ def generate_launch_description():
         PythonLaunchDescriptionSource(
             [
                 PathJoinSubstitution([
-                    FindPackageShare("vikings_bot_firmware_py"),
+                    FindPackageShare("vikings_bot_display_manager"),
                     "launch",
                     "display_manager.launch.py"
                 ])
